75851036e33c05d7ba5d3c29637c66a008e99166
Mackrina10 <heavenmhally@gmail.com>
2025-01-06 14:04:09 +0300

    quadrant

--- project.cpp
+++ project.cpp
@@ -6,4 +6,6 @@ int main () {
     std::random_devicerd;
     std::mt19937 gen(rd());
     // Normal distribution parameters: mean = 0.0, standard deviation = 1.0
-     std::normal_distribution<> dis(0.0, 
+     std::normal_distribution<> dis(0.0, 1.0);
+
+    // Generate and print 10 random numbers
