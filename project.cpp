1ec8bd1bffe4dec9fafd71764bdfb4950a508b3e
Mackrina10 <heavenmhally@gmail.com>
2025-01-06 14:02:00 +0300

    normal-actions

--- project.cpp
+++ project.cpp
@@ -6,4 +6,4 @@ int main () {
     std::random_devicerd;
     std::mt19937 gen(rd());
     // Normal distribution parameters: mean = 0.0, standard deviation = 1.0
-     std::normal_distribution<> dis(0
+     std::normal_distribution<> dis(0.0, 
